6863a118cf5de3becc4c8f131acc736a938814a8
josecarlosjimenezalonso <josecarlos.jimenez.alonso@alumnos.upm.es>
2017-11-14 04:21:04 +0100

    cambios de jc

--- gam.py
+++ gam.py
@@ -69,10 +69,88 @@ def check():
                 response.set_cookie('email', email)
                 return response
             else:
-                #response = make_response(render_template("index.html"))
-                #response.set_cookie('nombre', nombre)
-                response = make_response(principal())
-                response.set_cookie('nombre', nombre)
+                cursor = con.cursor()
+                print (email)
+                print(type(email))
+
+                cursor.execute("SELECT gastos.gasto FROM gastos WHERE gastos.email = '" + email + "' AND gastos.principal = 1")
+                gasto = cursor.fetchone()
+                gasto = gasto[0]
+
+                cursor.execute("SELECT reto FROM gastos WHERE gastos.email = '" + email + "' AND gastos.reto = 1")
+                reto = cursor.fetchone()
+                reto = reto[0]
+
+                cursor.execute("SELECT ingreso FROM ingresos WHERE email = '" + email + "' AND ingresos.principal = 1")
+                ingreso = cursor.fetchone()
+                ingreso = ingreso[0]
+                misgastos = gastos.calcula_gastos(ingreso, gasto, reto)
+
+                cursor.execute("SELECT gasto FROM gastos WHERE email = '" + email + "'")
+                resultadosgastos = cursor.fetchall()
+                ahorro = ingreso
+                for item in resultadosgastos:
+                    ahorro = ahorro - item[0]
+
+                cursor.execute("SELECT gasto FROM gastos WHERE gastos.email = '" + email + "' AND gastos.categoria = 'vivienda'")
+                vivienda = cursor.fetchone()
+                gastovivienda=0
+
+
+                if vivienda is None:
+                    gastovivienda = 0
+                else:
+                    for index in vivienda:
+                        gastovivienda = gastovivienda + vivienda[0]
+
+                cursor.execute("SELECT gasto FROM gastos WHERE gastos.email = '" + email + "' AND gastos.categoria = 'ocio'")
+                ocio = cursor.fetchone()
+                gastoocio = 0
+
+                if ocio is None:
+                    gastoocio = 0
+                else:
+                    for index in ocio:
+                        gastoocio = gastoocio + ocio[0]
+
+                cursor.execute("SELECT gasto FROM gastos WHERE gastos.email = '" + email + "' AND gastos.categoria = 'comida'")
+                comida = cursor.fetchone()
+                gastocomida = 0
+
+                if comida is None:
+                    gastocomida = 0
+                else:
+                    for index in comida:
+                        gastocomida = gastocomida + comida[0]
+
+                cursor.execute("SELECT gasto FROM gastos WHERE gastos.email = '" + email + "' AND gastos.categoria = 'otro'")
+                otro = cursor.fetchone()
+                gastootro = 0
+
+                if otro is None:
+                    gastootro = 0
+                else:
+                    for index in otro:
+                        gastootro = gastootro + otro[0]
+
+
+                gastosTotales = gastootro + gastocomida + gastoocio + gastovivienda
+
+                print (gastosTotales)
+
+                porcientoOtro = (gastootro * 100) / gastosTotales
+                porcientoComida = (gastocomida *100 ) / gastosTotales
+                porcientoOcio = (gastoocio * 100) / gastosTotales
+                porcientoVivienda = (gastovivienda *100 ) / gastosTotales
+
+                porcientoAhorro = ((ingreso-gastosTotales)*100)/ingreso
+
+                response = make_response(
+                    render_template("main/index.html",
+                                    vivienda=gastovivienda, comida=gastocomida, ocio=gastoocio, otros=gastootro, ahorro=ahorro
+                                    , porcientoComida = porcientoComida , porcientoOcio=porcientoOcio , porcientoOtro=porcientoOtro
+                                    , porcientoVivienda=porcientoVivienda,porcientoAhorro=porcientoAhorro ))
+
                 return response
 
         else:
@@ -92,14 +170,17 @@ def reto():
 
     cursor = con.cursor()
 
-    cursor.execute("INSERT INTO ingresos (ingreso , tipo, principal, ingresos.email) VALUES (%s,%s,1,%s)", (ingreso, 'nomina',email))
+    cursor.execute("INSERT INTO ingresos (ingreso , tipo, principal, email) VALUES (%s,%s,1,%s)", (ingreso, 'nomina',email))
     cursor.execute("INSERT INTO gastos (gasto , categoria, principal, reto, tipogasto, gastos.email) VALUES (%s,%s,1,0,%s,%s)", (gasto, 'vivienda', 'fijo', email))
     cursor.execute("INSERT INTO gastos (gasto , categoria, principal, reto, tipogasto, gastos.email) VALUES (%s,%s,0,1,%s,%s)", (ahorro, 'ahorro', 'ahorro',email))
 
     con.commit()
 
     #response = make_response(render_template("/PrimerosPasos/index.html"))
+
     response = make_response(calc_gastos())
+    cursor.execute("UPDATE users SET configuracioninicial=1 WHERE email='" + email + "'")
+    con.commit()
     return response
 
 
@@ -114,6 +195,7 @@ def calc_gastos():
     cursor = con.cursor()
     print (email2)
     print(type(email2))
+
     cursor.execute("SELECT gastos.gasto FROM gastos WHERE gastos.email = '" + email2 + "' AND gastos.principal = 1")
     gasto = cursor.fetchone()
     gasto = gasto[0]
@@ -122,14 +204,22 @@ def calc_gastos():
     reto = cursor.fetchone()
     reto = reto[0]
 
-    cursor.execute("SELECT ingreso FROM ingresos WHERE ingresos.email = '" + email2 + "' AND ingresos.principal = 1")
+    cursor.execute("SELECT ingreso FROM ingresos WHERE email = '" + email2 + "' AND ingresos.principal = 1")
     ingreso = cursor.fetchone()
     ingreso = ingreso[0]
     misgastos = gastos.calcula_gastos(ingreso, gasto, reto)
 
+    cursor.execute("SELECT gasto FROM gastos WHERE email = '" + email2 + "'")
+    resultado = cursor.fetchall()
+
     print(misgastos)
-    
-    response = make_response(render_template("main/index.html", vivienda=misgastos[0], comida=misgastos[1],casa=misgastos[2], ocio=misgastos[3], otros=misgastos[4]))
+
+    ahorro = ingreso
+
+    for item in resultado:
+        ahorro = ahorro - resultado[0]
+
+    response = make_response(render_template("main/index.html", vivienda=misgastos[0], comida=misgastos[1],casa=misgastos[2], ocio=misgastos[3], otros=misgastos[4], ahorro=ahorro))
 
     return response
 
@@ -152,9 +242,6 @@ def principal():
 
     return response
 
-@app.route("/principal", methods=['POST'])
-def inicio():
-    return render_template("main/index.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
